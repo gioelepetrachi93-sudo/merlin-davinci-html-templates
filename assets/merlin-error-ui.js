a201d1332fe6c570c12d491e439903ceba12d5ec
Gioele Petrachi <gioele.petrachi93@gmail.com>
2026-07-02 16:40:43 +0200

    mail/sms non in our record msg fix

--- assets/merlin-error-ui.js
+++ assets/merlin-error-ui.js
@@ -687,6 +687,18 @@
     isRenderingRecords = true;
 
     const copy = type === "phone"
+
+      ? {
+          title: "We are getting your details ready in My Merlin",
+          body: "Please try again in few hours.."
+        }
+      : {
+          title: "We are getting your details ready in My Merlin",
+          body: "Please try again in few hours."
+        };
+
+/*
+
       ? {
           title: "This mobile number is not in our records",
           body: "We don't recognise this mobile number. Please check it's entered correctly, or try a different one."
@@ -695,6 +707,8 @@
           title: "This email address is not in our records",
           body: "We don't recognise this email address. Please check it's entered correctly, or try a different one."
         };
+
+*/
 
     if (type === "phone") {
       target.className = "ml-flow-error merlin-records-error";
@@ -709,7 +723,7 @@
     target.innerHTML =
       getRecordsIconSvg() +
       "<div>" +
-      '<p class="merlin-records-error__title">' + copy.title + "</p>" +
+      '<p class="merlin-records-error__body">' + copy.title + "</p>" +
       '<p class="merlin-records-error__body">' + copy.body + "</p>" +
       "</div>";
 
